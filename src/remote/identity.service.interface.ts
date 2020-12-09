80685c16a6a3b26fb07c2c955941e83dbc100963
Jacob Saur <jsaur@kiva.org>
2020-12-09 17:22:35 +0100

    update npm packages and fix
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- src/remote/identity.service.interface.ts
+++ src/remote/identity.service.interface.ts
@@ -1,6 +1,6 @@
 export abstract class IIdentityService {
-    abstract async verifyFingerprint(position: number, image: string, filters: any): Promise<any>;
-    abstract async verifyFingerprintTemplate(position: number, template: string, filters: any): Promise<any>;
-    abstract async templatize(data: any): Promise<any>;
-    abstract async qualityCheck(filters: any): Promise<any>;
+    abstract verifyFingerprint(position: number, image: string, filters: any): Promise<any>;
+    abstract verifyFingerprintTemplate(position: number, template: string, filters: any): Promise<any>;
+    abstract templatize(data: any): Promise<any>;
+    abstract qualityCheck(filters: any): Promise<any>;
 }
--- src/remote/jwks.service.interface.ts
+++ src/remote/jwks.service.interface.ts
@@ -1,7 +1,7 @@
 import { CertSigningKey, SigningKey } from 'jwks-rsa';
 
 export abstract class IJwksService {
-    abstract async getKey(token: string): Promise<SigningKey>;
+    abstract getKey(token: string): Promise<SigningKey>;
 
     public static isCertSigningKey(key: SigningKey): key is CertSigningKey {
         return (key as CertSigningKey).publicKey !== undefined;
