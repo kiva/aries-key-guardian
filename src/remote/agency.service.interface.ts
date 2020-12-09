14c769925ccef03d85a8241a50de4d88032c41c1
Jacob <jsaur@kiva.org>
2020-12-09 19:29:32 +0100

    Pro 1794 - support node 14 (#39)
    * switch to node 14
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update npm packages and fix
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update docker
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update circle
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * add pg back in
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * dont map volume in ci
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * run tests from server
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update circle ci to have local tests and prod container check
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * update
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- src/remote/agency.service.interface.ts
+++ src/remote/agency.service.interface.ts
@@ -2,5 +2,5 @@
  * This describes a sort of DSL for the Agency Service.
  */
 export abstract class IAgencyService {
-    abstract async spinUpAgent(walletId: string, walletKey: string, adminApiKey: string, seed: string, agentId: string): Promise<any>;
+    abstract spinUpAgent(walletId: string, walletKey: string, adminApiKey: string, seed: string, agentId: string): Promise<any>;
 }
--- src/remote/sms.service.interface.ts
+++ src/remote/sms.service.interface.ts
@@ -1,3 +1,3 @@
 export abstract class ISmsService {
-    abstract async sendOtp(toNumber: string, otp: number): Promise<void>;
+    abstract sendOtp(toNumber: string, otp: number): Promise<void>;
 }
