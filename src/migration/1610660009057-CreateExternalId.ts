77e036520fac0b739b6a77086949c9190d90d332
Jeff Kennedy <jeffk@kiva.org>
2021-01-14 14:15:29 -0800

    Create new external_id table migration.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

---
+++ src/migration/1610660009057-CreateExternalId.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateExternalId1610660009057 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        // Creates primary key + index named "external_id_pkey"
+        // Creates a unique constraint named "external_id_id_external_id_type_key" and btree index on (id, external_id_type)
+        queryRunner.query(
+            `CREATE TABLE IF NOT EXISTS external_id (
+              id SERIAL PRIMARY KEY,
+              did VARCHAR(32) NOT NULL,
+              external_id TEXT NOT NULL,
+              external_id_type VARCHAR(40) NOT NULL,
+              UNIQUE (id, external_id_type)
+            );`
+        );
+        queryRunner.query('CREATE INDEX idx_external_id_did ON external_id(did);');
+        queryRunner.query('CREATE INDEX idx_external_id_external_id ON external_id(external_id);');
+        queryRunner.query('CREATE INDEX idx_external_id_external_id_type ON external_id(external_id_type);');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query('DROP INDEX IF EXISTS idx_external_id_external_id_type;');
+        queryRunner.query('DROP INDEX IF EXISTS idx_external_id_external_id;');
+        queryRunner.query('DROP INDEX IF EXISTS idx_external_id_did;');
+        queryRunner.query('DROP TABLE IF EXISTS external_id;');
+    }
+
+}
