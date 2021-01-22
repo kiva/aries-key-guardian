daa873237ed15c3ec16fdaeed20affb79b16a956
Jeff Kennedy <jeffk@kiva.org>
2021-01-21 16:53:21 -0800

    Need DbModule imported in PluginModule.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- src/plugins/plugin.module.ts
+++ src/plugins/plugin.module.ts
@@ -3,12 +3,14 @@ import { SmsModule } from '../sms/sms.module';
 import { PluginFactory } from './plugin.factory';
 import { RemoteModule } from '../remote/remote.module';
 import { TokenModule } from '../token/token.module';
+import { DbModule } from '../db/db.module';
 
 /**
  * Module for our different authentication plugins
  */
 @Module({
     imports: [
+        DbModule,
         RemoteModule,
         SmsModule,
         TokenModule,
