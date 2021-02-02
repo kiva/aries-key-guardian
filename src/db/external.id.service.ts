aae352b4b3d687bd5415ec4bb15175bb2ee7e6a0
Jeff Kennedy <jeffk@kiva.org>
2021-02-02 09:59:54 -0800

    Adding new IDs should only add if it doesn't already exist. Adding a plugin, for example, might use an existing ID.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- src/db/external.id.service.ts
+++ src/db/external.id.service.ts
@@ -74,4 +74,41 @@ export class ExternalIdService {
         }
         return this.externalIdRepository.save(externalIds);
     }
+
+    private async getOrCreateExternalId(externalId: ExternalId): Promise<ExternalId> {
+        const dbExternalId = await this.externalIdRepository.findOne({
+            did: externalId.did,
+            external_id: externalId.external_id,
+            external_id_type: externalId.external_id_type
+        });
+        return dbExternalId ?? await this.externalIdRepository.save(externalId);
+    }
+
+    public async getOrCreateExternalIds(did: string, filters: CreateFiltersDto): Promise<Array<ExternalId>> {
+        const externalIds: ExternalId[] = Array.from(filters.externalIds?.entries() ?? []).map((entry: [string, string]) => {
+            const externalId = new ExternalId();
+            externalId.did = did;
+            externalId.external_id = entry[1];
+            externalId.external_id_type = entry[0];
+            return externalId;
+        });
+        // TODO: Remove these two checks once we've removed the deprecated code (PRO-2676)
+        if (filters.govId1 || filters.nationalId) {
+            const idValue: string = filters.govId1 ?? filters.nationalId;
+            const externalId1 = new ExternalId();
+            externalId1.did = did;
+            externalId1.external_id = SecurityUtility.hash32(idValue + process.env.HASH_PEPPER);
+            externalId1.external_id_type = 'sl_national_id'; // TODO: update to be specifiable by the filters
+            externalIds.push(externalId1);
+        }
+        if (filters.govId2 || filters.voterId) {
+            const idValue: string = filters.govId2 ?? filters.voterId;
+            const externalId2 = new ExternalId();
+            externalId2.did = did;
+            externalId2.external_id = SecurityUtility.hash32(idValue + process.env.HASH_PEPPER);
+            externalId2.external_id_type = 'sl_voter_id'; // TODO: update to be specifiable by the filters
+            externalIds.push(externalId2);
+        }
+        return Promise.all(externalIds.map((externalId: ExternalId) => this.getOrCreateExternalId(externalId)));
+    }
 }
