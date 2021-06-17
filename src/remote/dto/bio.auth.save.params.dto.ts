be3e89ddd7712c1a94d7c04d9b642ccd731c3a66
Jeff Kennedy <jeffk@kiva.org>
2021-06-17 09:46:37 -0700

    Add position to BioAuthSaveParamsDto
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- src/remote/dto/bio.auth.save.params.dto.ts
+++ src/remote/dto/bio.auth.save.params.dto.ts
@@ -1,5 +1,6 @@
 import { FingerprintTypeEnum } from '../fingerprint.type.enum';
 import { IsEnum, IsInt, IsISO8601, IsNumber, IsOptional, IsString, Length } from 'class-validator';
+import { FingerprintPositionEnum } from '../fingerprint.position.enum';
 
 /**
  * DTO for the params of each fingerprint in the body of the request sent to Bio Auth Service's /save endpoint
@@ -7,6 +8,7 @@ import { IsEnum, IsInt, IsISO8601, IsNumber, IsOptional, IsString, Length } from
 export class BioAuthSaveParamsDto {
     @IsInt() readonly type_id: number;
     @IsISO8601() readonly capture_date: string;
+    @IsEnum(FingerprintPositionEnum) readonly position: FingerprintPositionEnum;
     @IsOptional() @IsString() readonly image?: string;
     @IsOptional() @IsString() readonly template?: string;
     @IsOptional() @IsNumber() readonly quality_score?: number;
