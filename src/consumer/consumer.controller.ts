21ed6fb37df1696f9381a674b49f6fc6bf682e1d
Derek Bullard <kingdgamer885@gmail.com>
2021-08-10 17:11:07 -0400

    Added networks to kafka and zookeeper, chnaged console.log to Logger in the consumer controller
    Signed-off-by: Derek Bullard <kingdgamer885@gmail.com>

--- src/consumer/consumer.controller.ts
+++ src/consumer/consumer.controller.ts
@@ -3,12 +3,14 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateConsumerDto } from './create-consumer.dto';
 import { Client, ClientKafka } from '@nestjs/microservices';
 import { kafkaConfig } from '../kafkaConfig';
+import { Logger } from 'protocol-common/logger';
+
 
 @Controller('posts')
 export class ConsumerController {
     @MessagePattern('escrow-create')
     getKafka(@Payload() message) {
-        console.log(message.value);
+        Logger.log(message.value);
         return 'Hello World';
     }
     
